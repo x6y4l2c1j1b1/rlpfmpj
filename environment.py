ca98b1727130f5de15b43b88faf3f31ef1ea3e25
xx2241 <xx2241@columbia.edu>
2017-11-17 17:01:14 -0500

    few changes

--- environment.py
+++ environment.py
@@ -1,7 +1,7 @@
 import numpy as np
 from preprocess import preprocess
 
-def build_enrironment(high_beta_stock_index,low_beta_stock_index):
+def build_enrironment(high_beta_stock_index,low_beta_stock_index, smooth_index):
 	"""
 	input:
 		chose a portfolio of one high_beta stock and one low_beta stock
@@ -14,7 +14,7 @@ def build_enrironment(high_beta_stock_index,low_beta_stock_index):
 	state:
 		total portfolio value, suppose we have 1000000 dollars
 		discrete allocations of two stocks, [0/10,1/9, 2/8, 3/7, 4/6, 5/5, 6/4, 7/3, 8/2, 9/1,10/0], rounded to the nearest
-		previous 2 history prices change, 0: not change too much(varies smaller than 0.5), 1: low_beta rise2: low_beta get down
+		previous 2 history prices change, 0: not change too much(varies smaller than smooth_index), 1: rise 2: get down
 		not added yet: left-over cash
 
 		how to encode state: 
@@ -30,7 +30,7 @@ def build_enrironment(high_beta_stock_index,low_beta_stock_index):
 
 	env:
 		env[state] is a dictionary
-		env[state][action] is a tuple of reward (next state is fixed knowing the history and low_beta_percentage, when running the test time series, next state will generate with respect to the test data)
+		env[state][action] is reward (next state is fixed knowing the history and low_beta_percentage, when running the test time series, next state will generate with respect to the test data)
 		env[state]['cnt'] is the times each state appears
 
 	"""
@@ -44,29 +44,29 @@ def build_enrironment(high_beta_stock_index,low_beta_stock_index):
 		l2=0
 		h3=0
 		l3=0
-		if price[high_beta_stock_index,i-2] - price[high_beta_stock_index,i-3] > 0.5:
+		if price[high_beta_stock_index,i-2] - price[high_beta_stock_index,i-3] > smooth_index:
 			h1 = 1
-		elif price[high_beta_stock_index,i-2] - price[high_beta_stock_index,i-3]  < -0.5:
+		elif price[high_beta_stock_index,i-2] - price[high_beta_stock_index,i-3]  < -smooth_index:
 			h1 = 2
-		if price[low_beta_stock_index,i-2] - price[low_beta_stock_index,i-3] > 0.5:
+		if price[low_beta_stock_index,i-2] - price[low_beta_stock_index,i-3] > smooth_index:
 			l1 = 1
-		elif price[low_beta_stock_index,i-2] - price[low_beta_stock_index,i-3]  < -0.5:
+		elif price[low_beta_stock_index,i-2] - price[low_beta_stock_index,i-3]  < -smooth_index:
 			l1 = 2
-		if price[high_beta_stock_index,i-1] - price[high_beta_stock_index,i-2] > 0.5:
+		if price[high_beta_stock_index,i-1] - price[high_beta_stock_index,i-2] > smooth_index:
 			h2 = 1
-		elif price[high_beta_stock_index,i-1] - price[high_beta_stock_index,i-2]  < -0.5:
+		elif price[high_beta_stock_index,i-1] - price[high_beta_stock_index,i-2]  < -smooth_index:
 			h2 = 2
-		if price[low_beta_stock_index,i-1] - price[low_beta_stock_index,i-2] > 0.5:
+		if price[low_beta_stock_index,i-1] - price[low_beta_stock_index,i-2] > smooth_index:
 			l2 = 1
-		elif price[low_beta_stock_index,i-1] - price[low_beta_stock_index,i-2]  < -0.5:
+		elif price[low_beta_stock_index,i-1] - price[low_beta_stock_index,i-2]  < -smooth_index:
 			l2 = 2
-		if price[high_beta_stock_index,i] - price[high_beta_stock_index,i-1] > 0.5:
+		if price[high_beta_stock_index,i] - price[high_beta_stock_index,i-1] > smooth_index:
 			h3 = 1
-		elif price[high_beta_stock_index,i] - price[high_beta_stock_index,i-1]  < -0.5:
+		elif price[high_beta_stock_index,i] - price[high_beta_stock_index,i-1]  < -smooth_index:
 			h3 = 2
-		if price[low_beta_stock_index,i] - price[low_beta_stock_index,i-1] > 0.5:
+		if price[low_beta_stock_index,i] - price[low_beta_stock_index,i-1] > smooth_index:
 			l3 = 1
-		elif price[low_beta_stock_index,i] - price[low_beta_stock_index,i-1]  < -0.5:
+		elif price[low_beta_stock_index,i] - price[low_beta_stock_index,i-1]  < -smooth_index:
 			l3 = 2
 		history = h1*1000 + l1*100 + h2*10 + l2
 		total_value = 1000000
@@ -79,7 +79,7 @@ def build_enrironment(high_beta_stock_index,low_beta_stock_index):
 				cnt = env[state_index]['cnt']
 				for action in range(-min(3, low_beta_percent), min(10-low_beta_percent, 3)):
 					new_low_beta_percent = low_beta_percent + action
-					reward = total_value * (new_low_beta_percent * 0.1 * price[low_beta_stock_index,i] + (10-new_low_beta_percent) * 0.1 * price[high_beta_stock_index,i])/(low_beta_percent * 0.1 * price[low_beta_stock_index,i-1] + (10-low_beta_percent) * 0.1 * price[high_beta_stock_index,i-1]) - total_value
+					reward = total_value * (new_low_beta_percent * 0.1 * price[low_beta_stock_index,i]/price[low_beta_stock_index,i-1] + (10-new_low_beta_percent) * 0.1 * price[high_beta_stock_index,i]/price[high_beta_stock_index,i-1]) - total_value	
 					env[state_index][action] = (env[state_index][action] * cnt + reward)/(cnt+1)
 					env[state_index]['cnt'] += 1
 			else:
@@ -87,18 +87,14 @@ def build_enrironment(high_beta_stock_index,low_beta_stock_index):
 				env[state_index]['cnt'] = 1
 				for action in range(-min(3, low_beta_percent), min(10-low_beta_percent, 3)):
 					new_low_beta_percent = low_beta_percent + action
-					reward = total_value * (new_low_beta_percent * 0.1 * price[low_beta_stock_index,i] + (10-new_low_beta_percent) * 0.1 * price[high_beta_stock_index,i])/(low_beta_percent * 0.1 * price[low_beta_stock_index,i-1] + (10-low_beta_percent) * 0.1 * price[high_beta_stock_index,i-1]) - total_value
-					#print new_low_beta_percent
-					#print (price[low_beta_stock_index,i] * total_value * 0.1 * low_beta_percent)/price[low_beta_stock_index,i-1]
-					#print (price[high_beta_stock_index,i] * total_value * 0.1 *(10-low_beta_percent))/price[high_beta_stock_index,i-1]
-					#reward = (price[low_beta_stock_index,i] * total_value * 0.1 * new_low_beta_percent) + (price[high_beta_stock_index,i] * total_value * 0.1 *(10-new_low_beta_percent)) - total_value
-					print reward
+					reward = total_value * (new_low_beta_percent * 0.1 * price[low_beta_stock_index,i]/price[low_beta_stock_index,i-1] + (10-new_low_beta_percent) * 0.1 * price[high_beta_stock_index,i]/price[high_beta_stock_index,i-1]) - total_value
+					
 					env[state_index][action] = reward
 	return env
 
-env = build_enrironment(1,11)
+env = build_enrironment(1,11,0.7)
 print env
-print env[1031212]
+print env.keys() ## some keys may not exist since we may never come across that scenario
 
 					
 

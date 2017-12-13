600b1324109f16e1331f37ea65c3de9dc1135aef
xx2241 <xx2241@columbia.edu>
2017-12-13 14:20:58 -0500

    random choose

--- run_this.py
+++ run_this.py
@@ -87,6 +87,44 @@ def test_stock():
         print('total_value: %d, profit_estimation: %d' % (observation[0], profit_estimation))
     print('test, win: %d, lose: %d, profit_estimation: %d' % (win, lose, profit_estimation))
 
+def run_random():
+    step = 0
+    win = 0
+    lose = 0
+    total_episode = 100
+    profit_estimation = 0
+    for episode in range(total_episode):
+        # initial observation
+        print episode
+        env.reset(np.random.randint(0,10), np.random.randint(0,10))
+        #observation = env.reset()
+        observation = np.array(env.state)
+        while True:
+            # fresh env
+            #env.render()
+
+            # RL choose action based on observation
+            action = np.random.randint(0,7)
+            # RL take action and get next observation and reward
+            observation_, reward, done = env.take_action(action)
+            observation_ = np.array(observation_)
+            # swap observation
+            observation = observation_
+
+            # break while loop when end of this episode
+            if done:
+                break
+            step += 1
+        if observation[0] >= 1000000:
+            win += 1
+        else:
+            lose += 1
+        profit_estimation = (profit_estimation * episode + (observation[0] - 1000000.))/(episode+1.)
+
+    # end of game
+        print('total_value: %d, profit_estimation: %d' % (observation[0], profit_estimation))
+    print('random, win: %d, lose: %d, profit_estimation: %d' % (win, lose, profit_estimation))
+
 
 if __name__ == "__main__":
     # maze game
@@ -101,6 +139,8 @@ if __name__ == "__main__":
                       memory_size=2000,
                       # output_graph=True
                       )
-    run_stock()
-    test_stock()
+    
+    # run_stock()
+    # test_stock()
+    run_random()
     RL.plot_cost()
